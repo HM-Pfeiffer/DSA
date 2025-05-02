aca9de2f31a7626ddc532b22f4e7f3d9f258b9cc
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-01 22:13:45 -0400

    Time: 12 ms (73.52%) | Memory: 73.5 MB (20.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const myNumsSet = new Set();
+
+    for(const num of nums){
+        if (myNumsSet.has(num)){
+            return true;
+        }
+        myNumsSet.add(num);
+    }
+    return false;
+};
