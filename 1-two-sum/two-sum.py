77dfc0d9be97ed3916e9d6be2937598a59cd1b32
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-01 21:57:59 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (24.72%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int] | str:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         cache = {}
         for i, num in enumerate(nums):
             complement = target - num
