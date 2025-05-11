51c2b19652735e35bfe0b2c6c61e80b1bc8ac03b
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-10 22:03:02 -0400

    Update valid-palindrome.js

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,7 +5,6 @@
 var isPalindrome = function(s) {
     s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     const mid_len = Math.floor(s.length/2);
-
     for(let i = 0; i < mid_len; i++){
         if(s[i] !== s[s.length - 1 - i]){
             return false;
