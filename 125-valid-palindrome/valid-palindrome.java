a4181849567a89ea9e0d4bf570d376a6cdee39b7
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-10 21:51:12 -0400

    Time: 6 ms (46.50%) | Memory: 43.6 MB (53.63%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isBlank()) return true;
+        StringBuilder clean_sb = stringDataCleaner(s);
+        StringBuilder clean_reversed_sb = stringDataCleaner(s).reverse();
+        return clean_sb.compareTo(clean_reversed_sb) == 0 ? true : false;
+    }
+
+    public StringBuilder stringDataCleaner(String s){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                char c = Character.toLowerCase(s.charAt(i));
+                sb.append(c);
+            }
+        }
+        return sb;
+    }
+}
