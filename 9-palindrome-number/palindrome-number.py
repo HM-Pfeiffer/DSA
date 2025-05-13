612fd81f05f2503a7411caf52c816c21b0934150
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-12 23:24:03 -0400

    Update palindrome-number.py

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,16 +4,3 @@ class Solution:
             return False
         return x == int(str(x)[::-1])
 
-
-# alternatively logic
-# if x < 0: return False
-
-# str_num = str(x)
-# mid_len = len(str_num)
-
-# for i in range(mid_len):
-#     if str_num[i] != str_num[-1-i]:
-#         return False
-
-# return True
-
