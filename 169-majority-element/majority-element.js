2ca0213215ab5eeaad22130a89ae242d31798a78
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-13 12:03:22 -0400

    Time: 5 ms (47.32%) | Memory: 57.1 MB (56.95%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let maxCount = 0;
+    let majority = -1;
+    const map = new Map();
+
+    for(const n of nums){
+       let freq = (map.get(n) || 0) + 1;
+        map.set(n, freq);
+
+        if(freq > maxCount){
+            maxCount = freq; 
+            majority = n;
+        }
+    }
+    return majority;
+};
