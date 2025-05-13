c7018209bc427807bf439e36341382bad04508bc
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-13 11:41:41 -0400

    Time: 15 ms (22.07%) | Memory: 49.6 MB (80.09%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums){
+            int total = map.getOrDefault(n, 0) + 1;
+            map.put(n, total);
+        }
+        
+        System.out.println(map.toString());
+        int max_count = 0;
+        int majority = -1;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > max_count){
+                majority = entry.getKey();
+                max_count = entry.getValue();
+            }
+        }
+        return majority;
+    }
+}
