2d443457dabbe143abc2ed725e84d20a025ecca3
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-12 23:02:52 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (33.81%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -7,6 +7,7 @@ class Solution {
         if(haystackLength == 1){
             return haystack.equals(needle) ? 0 : -1;
         }
+        // run until the last index of needle + 1 is less than haystack length for accurate substring 
         for(int i = 0; (i + needleLength - 1) < haystackLength; i++){
             substring = haystack.substring(i, needleLength + i);
             if(substring.equals(needle)){
