d9a5d3796a72fb3bf1a5bb14c3138f29fccfb335
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-20 12:10:39 -0400

    Time: 337 ms (87.17%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-  
-SELECT DISTINCT v.author_id id
-FROM Views v
-WHERE v.author_id = v.viewer_id
-ORDER BY id;
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT author_id AS id
+FROM views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC;
