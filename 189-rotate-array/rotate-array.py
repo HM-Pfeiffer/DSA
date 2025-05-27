75775b7382713a8c1776fa6fee638449e51aee54
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-26 22:25:53 -0400

    Time: 3 ms (74.35%) | Memory: 25.8 MB (9.69%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,11 +1,10 @@
 class Solution:
-
     def reverse(self, nums: List[int], l: int, r: int) -> List[int]:
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
+        while l < r:    # iterate until two points cross at the middle
+            nums[l], nums[r] = nums[r], nums[l]   # In Python has 'under-the-hood temp var' that allows a one-liner to swap variables 
+            l += 1  # purposely updating l and r to separately lines to reduce need for under-the-hood tuple
             r -= 1
-    
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -14,10 +13,10 @@ class Solution:
             return
 
         length = len(nums)
-        k %= length
-        self.reverse(nums, 0, length - 1)
-        self.reverse(nums, 0, k - 1)
-        self.reverse(nums, k, length - 1)
+        k %= length                          # Step 1: adjust for multiple rotations 
+        self.reverse(nums, 0, length - 1)    # Step 2: reverse whole input array
+        self.reverse(nums, 0, k - 1)         # Step 3: reverse first half of array
+        self.reverse(nums, k, length - 1)    # Step 4: reverse latter half of array
         
 
 
