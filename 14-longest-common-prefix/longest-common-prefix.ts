3a86a24736bbfa9afea829bf52b90e34a90183d4
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-28 22:45:44 -0400

    Time: 3 ms (39.92%) | Memory: 56.7 MB (40.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,8 +1,9 @@
 function longestCommonPrefix(strs: string[]): string {
     let prefix: string = "";
     const shortest_word: string = strs.reduce((a, b) => (a.length < b.length ? a : b));
+    const shortest_word_length: number = shortest_word.length;
 
-    for(let i = 0; i < shortest_word.length; i++){
+    for(let i = 0; i < shortest_word_length; i++){
         for (const word of strs){
             if(word[i] != shortest_word[i]){
                 return prefix;
