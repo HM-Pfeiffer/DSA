e7e46ed67a77ee57dcfc86463bc0dd45b9d1d9a2
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-29 14:35:51 -0400

    Time: 70 ms (14.71%) | Memory: 65.5 MB (5.11%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,6 +1,4 @@
 function twoSum(nums: number[], target: number): number[] {
-    let l: number = 0;
-    let r: number = 0; 
     const cache = new Map<number, number>();
 
     for (let i: number = 0; i < nums.length; i++){
