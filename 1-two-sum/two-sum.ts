8a670185e972d58fa1b37fac2b84eb339edb8ad7
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-29 14:36:32 -0400

    Time: 1 ms (88.75%) | Memory: 57.7 MB (28.45%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -3,7 +3,6 @@ function twoSum(nums: number[], target: number): number[] {
 
     for (let i: number = 0; i < nums.length; i++){
         let complement: number = target - nums[i];
-        console.log(complement);
         if (cache.has(complement)){
             return [cache.get(complement), i];
         }
