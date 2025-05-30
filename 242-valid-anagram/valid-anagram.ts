25697a674680184e68b695861a04f1660de28059
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-05-30 17:22:02 -0400

    Time: 7 ms (88.63%) | Memory: 57.1 MB (84.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,3 +1,19 @@
 function isAnagram(s: string, t: string): boolean {
-    return s.split('').sort().join('') == t.split('').sort().join('');
+    // check if the length of s and t are equal, if not return false
+    if (s.length != t.length) return false;
+
+    // create a new array with 26 characters (one for each of the letters of the alphabet)
+    const alphabet = new Array(26).fill(0);
+
+    // create a loop and increment count for every letter present in 
+    for(let i: number = 0; i < s.length; i++){
+        alphabet[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        alphabet[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    // check if all elements are 0
+    for(const n of alphabet){
+        if(n != 0) return false;
+    }
+    return true;
 };
