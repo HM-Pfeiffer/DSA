95ac345760ed1c7b644abfcbeff12772f3708cdb
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-06-01 16:02:56 -0400

    Time: 21 ms (13.43%) | Memory: 49.1 MB (12.40%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,11 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        if(strs.length == 0){
-            return new ArrayList();
-        }
-
-        Map<String, List> anagramsMap = new HashMap<>();
+        Map<String, List<String>> anagramsMap = new HashMap<>();
         int[] alphabet = new int[26];
 
         for(String s: strs){
@@ -17,6 +12,6 @@ class Solution {
             String key = Arrays.toString(alphabet);
             anagramsMap.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList(anagramsMap.values());
+        return new ArrayList<>(anagramsMap.values());
     }
 }
