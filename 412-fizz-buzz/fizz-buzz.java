6e066f0e2796f7b84fdd946838d4e63ab262ee6c
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-06-10 22:22:35 -0400

    Time: 1 ms (99.70%) | Memory: 44.6 MB (99.60%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -3,16 +3,12 @@ class Solution {
         List<String> output = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            StringBuilder str = new StringBuilder();
-
-            if (i % 3 == 0) {
-                str.append("Fizz");
-            }
-            if (i % 5 == 0) {
-                str.append("Buzz");
-            }
-            String result = str.isEmpty() ? Integer.toString(i) : str.toString();
-            output.add(result);
+            output.add(
+                i % 15 == 0 ? "FizzBuzz" :
+                i % 5 == 0 ? "Buzz" :
+                i % 3 == 0 ? "Fizz" :
+                String.valueOf(i)
+            );
         }
         return output;
     }
