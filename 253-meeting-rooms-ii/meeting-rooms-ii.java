5e3ec115b2166838d92825bc6f5c444ae963849d
Heather Pfeiffer <159729091+HM-Pfeiffer@users.noreply.github.com>
2025-06-11 22:30:22 -0400

    Time: 8 ms (66.53%) | Memory: 46 MB (65.08%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,24 @@
+import java.util.*;
+/**
+    Step 1: Sort by start time
+    Step 2: Min-heap to track the earliest end time
+    Step 3: Iterate through meetings
+            - If the room is free, remove the old end time
+            - Allocate room (add current meeting’s end time)
+    Step 4: Heap size is the number of rooms needed
+ */
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int[] interval : intervals) {
+            if (!heap.isEmpty() && heap.peek() <= interval[0]) {
+                heap.poll();
+            }
+            heap.offer(interval[1]);
+        }
+        return heap.size();
+    }
+}
